8709ee898a504b426cb7e151e39da92054c99036
aletlima <aletlima@gmail.com>
2022-06-07 10:54:45 -0300

    Remove tests from modules

--- CoffeeShop/Modules/Package.swift
+++ CoffeeShop/Modules/Package.swift
@@ -21,8 +21,6 @@ let package = Package(
         .target(
             name: "Modules",
             dependencies: []),
-        .testTarget(
-            name: "ModulesTests",
-            dependencies: ["Modules"]),
+        
     ]
 )
